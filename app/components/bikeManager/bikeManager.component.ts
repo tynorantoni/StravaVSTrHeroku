267124c0330591292294e92c4804bc38df48bced
Pawel Szymaszek <pawel.szymaszek@gmail.com>
2018-01-30 14:08:26 +0100

    making things brighter

---
+++ app/components/bikeManager/bikeManager.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Http } from "@angular/http";
+import 'rxjs/add/operator/map';
+
+@Component({
+    selector: 'bikeManager-component',
+    templateUrl: 'components/bikeManager/bikeManager.component.html',
+    styleUrls: ['components/bikeManager/bikeManager.component.css']
+})
+export class BikeManagerComponent {
+   
+    constructor(){}
+}
