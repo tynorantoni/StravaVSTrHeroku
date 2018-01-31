139a3c4ea71c2322f54b038ceef3d9207584a9d5
Pawel Szymaszek <pawel.szymaszek@gmail.com>
2018-01-31 01:27:32 +0100

    smooothing frontend prove of concept

--- app/app-routing-module.js
+++ app/app-routing-module.js
@@ -1,18 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var app_component_1 = require("./app.component");
 var about_component_1 = require("./components/about/about.component");
 var bikeManager_component_1 = require("./components/bikeManager/bikeManager.component");
 var page_not_found_component_1 = require("./components/pageNotFound/page-not-found.component");
+var strava_component_1 = require("./components/strava/strava.component");
 var router_1 = require("@angular/router");
 exports.routes = [
-    { path: '', component: app_component_1.AppComponent },
+    { path: '', component: strava_component_1.StravaComponent },
     { path: 'bikemanager', component: bikeManager_component_1.BikeManagerComponent },
     { path: 'about', component: about_component_1.AboutComponent },
     { path: 'not-found', component: page_not_found_component_1.PageNotFoundComponent },
     { path: '**', redirectTo: '/not-found' }
 ];
-exports.routing = router_1.RouterModule.forRoot(exports.routes, { useHash: true });
+exports.routing = router_1.RouterModule.forRoot(exports.routes, { useHash: false });
 //const appRoutes: Routes = [
 //  {path: '', component: AppComponent},
 //  {path: 'bikemanager', component: BikeManagerComponent},
--- app/app-routing-module.ts
+++ app/app-routing-module.ts
@@ -3,6 +3,7 @@ import { AboutComponent } from "./components/about/about.component";
 import { BikeManagerComponent } from "./components/bikeManager/bikeManager.component";
 import { MetarComponent } from "./components/metar/metar.component";
 import { PageNotFoundComponent } from "./components/pageNotFound/page-not-found.component";
+import { StravaComponent } from "./components/strava/strava.component";
 import {NgModule} from '@angular/core';
 
 import {RouterModule, Routes, Route} from '@angular/router';
@@ -10,14 +11,14 @@ import {RouterModule, Routes, Route} from '@angular/router';
 
 
 export const routes: Route[] = [
-    {path: '', component: AppComponent},
+    {path: '', component: StravaComponent},
   {path: 'bikemanager', component: BikeManagerComponent},
   {path: 'about', component: AboutComponent},
   {path: 'not-found', component: PageNotFoundComponent},
   {path: '**', redirectTo: '/not-found'}
 ];
 
-export const routing = RouterModule.forRoot(routes, { useHash: true });
+export const routing = RouterModule.forRoot(routes, { useHash: false });
 
 
 //const appRoutes: Routes = [
