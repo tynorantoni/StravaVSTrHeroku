7afc6d3018772d058f03e6295a8bd193373ec512
Pawel Szymaszek <pawel.szymaszek@gmail.com>
2018-02-11 00:29:49 +0100

    last fixes

---
+++ app/components/metar/metar.module.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var MetarModule = /** @class */ (function () {
+    function MetarModule() {
+    }
+    return MetarModule;
+}());
+exports.MetarModule = MetarModule;
+//# sourceMappingURL=metar.module.js.map
---
+++ app/components/strava/strava.module.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var StravaModule = /** @class */ (function () {
+    function StravaModule() {
+    }
+    return StravaModule;
+}());
+exports.StravaModule = StravaModule;
+//# sourceMappingURL=strava.module.js.map
